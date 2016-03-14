e2acdc79a68304898fd2ab89d737540a3e299128
murukessan <murukessanap@gmail.com>
2016-03-14 20:40:51 +0530

    after model urlMAp from json

--- collobSearch/admin.py
+++ collobSearch/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Searcher
 
-admin.site.register(UrlMap,KeyVal,Searcher)
+admin.site.register(UrlMap)
+admin.site.register(KeyVal)
+admin.site.register(Searcher)
 
--- collobSearch/admin.py~
+++ collobSearch/admin.py~
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Searcher
 
-admin.site.register(KeyVal,UrlMap,Searcher)
+admin.site.register(UrlMap,KeyVal,Searcher)
 
