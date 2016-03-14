1f944ea2b09943f50503080645c60da987e12327
murukessan <murukessanap@gmail.com>
2016-03-14 21:53:14 +0530

    after model urlMAp from json

--- collobSearch/models.py
+++ collobSearch/models.py
@@ -2,6 +2,21 @@ from django.db import models
 #from django.contrib.postgres.fields import JSONField
 #from django.utils import timezone
 
+class Searcher(models.Model):
+    #author = models.ForeignKey('auth.User')
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    areaOfInterest = models.CharField(max_length=50)
+    expertise = models.IntegerField()
+    #urlmap = models.ForeignKey(UrlMap,null=True, db_index=True)
+
+    def publish(self):
+        expertise = 0
+        self.save()
+
+    def __str__(self):
+        return self.username
+
 class UrlMap(models.Model):
     areaOfInterest  = models.CharField(max_length=50)
     username = models.ForeignKey(Searcher, db_index=True) 
@@ -17,19 +32,3 @@ class KeyVal(models.Model):
 
     def publish(self):
         self.save()
-
-
-class Searcher(models.Model):
-    #author = models.ForeignKey('auth.User')
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    areaOfInterest = models.CharField(max_length=50)
-    expertise = models.IntegerField()
-    #urlmap = models.ForeignKey(UrlMap,null=True, db_index=True)
-
-    def publish(self):
-        expertise = 0
-        self.save()
-
-    def __str__(self):
-        return self.username
