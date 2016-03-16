0927de06f979d23c87dfd02b252adceb0d4f0bf6
murukessan <murukessanap@gmail.com>
2016-03-16 11:36:39 +0530

    rcc change

--- collobSearch/models.py
+++ collobSearch/models.py
@@ -17,6 +17,21 @@ class Searcher(models.Model):
     def __str__(self):
         return self.username
 
+    def __iter__(self):
+        #return [field.value_to_string(self) for field in Searcher._meta.fields]
+        self.count = 0 
+        self.list =  [ self.username, 
+                       self.password, 
+                       self.areaOfInterest, 
+                       self.expertise ] 
+        return self
+
+    def __next__(self):
+      if self.count == len(self.list):
+        raise StopIteration
+      self.count = self.count + 1
+      return self.list[self.count - 1]
+
 class UrlMap(models.Model):
     areaOfInterest  = models.CharField(max_length=50)
     username = models.ForeignKey(Searcher, db_index=True, null=True) 
@@ -27,6 +42,19 @@ class UrlMap(models.Model):
     def __str__(self):
         return "UrlMap"
 
+    def __iter__(self):
+        #return [field.value_to_string(self) for field in UrlMap._meta.fields]
+        self.count = 0
+        self.list = [ self.areaOfInterest, 
+                       self.username ] 
+        return self
+
+    def __next__(self):
+      if self.count == len(self.list):
+        raise StopIteration
+      self.count = self.count + 1
+      return self.list[self.count - 1]
+
 class KeyVal(models.Model):
     aoi = models.ForeignKey(UrlMap, db_index=True)
     url = models.CharField(max_length=240, db_index=True)
@@ -36,4 +64,16 @@ class KeyVal(models.Model):
 
     def __str__(self):
         return "KeyVal"
+    
+    def __iter__(self):
+        #return [field.value_to_string(self) for field in KeyVal._meta.fields]
+        self.count = 0
+        self.list = [ self.aoi,
+                      self.url ]
+        return self
 
+    def __next__(self):
+      if self.count == len(self.list):
+        raise StopIteration
+      self.count = self.count + 1
+      return self.list[self.count - 1]
