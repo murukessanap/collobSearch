6d42346abf22485421be7c8d21d37adcc0d44480
murukessan <murukessanap@gmail.com>
2016-03-16 20:43:05 +0530

    model pucca

--- collobSearch/models.py
+++ collobSearch/models.py
@@ -11,69 +11,32 @@ class Searcher(models.Model):
     #urlmap = models.ForeignKey(UrlMap,null=True, db_index=True)
 
     def publish(self):
-        expertise = 0
+        self.expertise = 0
         self.save()
 
     def __str__(self):
         return self.username
 
-    def __iter__(self):
-        #return [field.value_to_string(self) for field in Searcher._meta.fields]
-        self.count = 0 
-        self.list =  [ self.username, 
-                       self.password, 
-                       self.areaOfInterest, 
-                       self.expertise ] 
-        return self
-
-    def __next__(self):
-      if self.count == len(self.list):
-        raise StopIteration
-      self.count = self.count + 1
-      return self.list[self.count - 1]
-
+   
 class UrlMap(models.Model):
     areaOfInterest  = models.CharField(max_length=50)
-    username = models.ForeignKey(Searcher, db_index=True, null=True) 
+    searcher = models.ForeignKey(Searcher, db_index=True, null=True) 
 
     def publish(self):
         self.save()
 
     def __str__(self):
-        return "UrlMap"
+        return self.areaOfInterest
 
-    def __iter__(self):
-        #return [field.value_to_string(self) for field in UrlMap._meta.fields]
-        self.count = 0
-        self.list = [ self.areaOfInterest, 
-                       self.username ] 
-        return self
-
-    def __next__(self):
-      if self.count == len(self.list):
-        raise StopIteration
-      self.count = self.count + 1
-      return self.list[self.count - 1]
+    
 
 class KeyVal(models.Model):
-    aoi = models.ForeignKey(UrlMap, db_index=True)
+    urlmap = models.ForeignKey(UrlMap, db_index=True)
     url = models.CharField(max_length=240, db_index=True)
 
     def publish(self):
         self.save()
 
     def __str__(self):
-        return "KeyVal"
-    
-    def __iter__(self):
-        #return [field.value_to_string(self) for field in KeyVal._meta.fields]
-        self.count = 0
-        self.list = [ self.aoi,
-                      self.url ]
-        return self
-
-    def __next__(self):
-      if self.count == len(self.list):
-        raise StopIteration
-      self.count = self.count + 1
-      return self.list[self.count - 1]
+        return self.url
+   
