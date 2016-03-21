80b11ef82a5aeb9688ed71b564c6c3a3bef2a1c0
murukessan <murukessanap@gmail.com>
2016-03-21 03:45:11 +0000

    model urls display ok

--- collobSearch/views.py
+++ collobSearch/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from collobSearch.models import KeyVal
+from collobSearch.models import UrlMap
+from collobSearch.models import Searcher
 
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'collobSearch/index.html', {})
+    #urls=KeyVal.objects.all()
+    data=[]
+    searchers=Searcher.objects.all()
+    for s in searchers:
+        urlMaps=UrlMap.objects.filter(searcher=s)
+        for um in urlMaps:
+            urls=KeyVal.objects.filter(urlmap=um)
+            for u in urls:
+                data.append((s.username,um.areaOfInterest,u.url))
+    #print(data)
+    #searchers=Searcher.objects.all()
+    return render(request, 'collobSearch/index.html', {'data':data})
